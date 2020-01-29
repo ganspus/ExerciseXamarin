51de05d5fda093f24a824843aa638b54172d83f3
Ganispuspitah <ganis.puspita@integraldp.co.id>
2020-01-29 09:23:12 +0700

    290120

--- Views/Exercise7Page.xaml.cs
+++ Views/Exercise7Page.xaml.cs
@@ -12,12 +12,6 @@ namespace ExerciseXamarin.Views
     {
 		private MovieService _service = new MovieService();
 
-		// Note that IsSearching is a bindable property. This is required for 
-		// binding ActivityIndicator's IsRunning property. If we do not define 
-		// IsSearching as a bindable property, its initial value (false) will 
-		// be used to set ActivityIndicator.IsRunning. Later when we change
-		// the value of IsSearching, ActivityIndicator will be unaware of this. 
-		// So, we need to implement it as a bindable property. 
 		private BindableProperty IsSearchingProperty =
 			BindableProperty.Create("IsSearching", typeof(bool), typeof(Exercise7Page), false);
 		public bool IsSearching
@@ -35,32 +29,14 @@ namespace ExerciseXamarin.Views
 
 		async void OnTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
 		{
-			// Here we are checking for "null" because if the user presses Cancel, 
-			// e.NewTextValue will be null. 
 			if (e.NewTextValue == null || e.NewTextValue.Length < MovieService.MinSearchLength)
 				return;
 
-			// Note that I've movied the logic of setting the ListView's ItemSource
-			// and handling errors to LoadMovies method. The reason for this is 
-			// because try/catch blocks should not be in the middle of your methods. 
-			// They make the code look ugly. 
 			await FindMovies(actor: e.NewTextValue);
 		}
 
 		async Task FindMovies(string actor)
 		{
-			// Clean coding tip: try/catch should NOT be in the middle 
-			// of a method. 
-
-			// The responsibility of this method is quite clear. 
-			// It calls the service and displays the result (either movies 
-			// in the ListView or a Label). If anything unexpected happens,
-			// it prevents the application from crashing and displays an alert. 
-			// If we were to call this method from two different places, we 
-			// wouldn't have to do error handling or setting IsSearching in
-			// two different places. All these go hand-in-hand with setting
-			// ListView's ItemSource. That's why I've encapsulated all that 
-			// in one method. 
 			try
 			{
 				IsSearching = true;
